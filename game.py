43c173702867de4c65775c9289bd35db9b499a57
MatviiChernenko <chernenkomat123@gmail.com>
2025-10-25 13:36:00 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,14 +1,13 @@
 # напиши тут код основного вікна гри
 from mapmanager import Mapmaneger
+from hero import Hero
 from direct.showbase.ShowBase import ShowBase
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.maps = Mapmaneger()
+        self.maps.load_land()
+        self.hero = Hero()
 
 game = Game()
-game.maps.addBlock((0,0,0))
-game.maps.addBlock((0,0,1))
-game.maps.addBlock((0,1,0))
-game.maps.addBlock((1,1,0))
 game.run()
--- hero.py
+++ hero.py
@@ -1,1 +1,8 @@
 # напиши свій код тут
+class Hero():
+    def __init__(self):
+        self.hero = loader.loadModel("block.egg")
+        self.hero.setTexture(loader.loadTexture("block.png"))
+        self.hero.setColor((0.8, 0.9, 0 , 1))
+        self.hero.setPos(10,20,3)
+        self.hero.reparentTo(render)
