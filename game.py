e70fc7b86bbbf0a7373d2236045a24de206ef561
MatviiChernenko <chernenkomat123@gmail.com>
2025-11-08 13:34:41 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -2,12 +2,22 @@
 from mapmanager import Mapmaneger
 from hero import Hero
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.maps = Mapmaneger()
         self.maps.load_land()
         self.hero = Hero()
+        self.task_mgr.add(self.update,"update")
+        props = WindowProperties()
+        props.setCursorHidden(True)
+        base.win.requestProperties(props)
+
+    def update(self,task):
+        self.hero.update()
+        return task.cont
 
 game = Game()
 game.run()
--- hero.py
+++ hero.py
@@ -1,4 +1,5 @@
 # напиши свій код тут
+from math import *
 
 camera = "c"
 mode = "m"
@@ -12,10 +13,22 @@ rotate_right = "arrow_right"
 up = "q"
 down = "e"
 
+def degreeToRadian(angle):
+    return angle *(pi / 180)
+
 class Hero():
     def __init__(self):
         self.cameraOn = True
         self.mode = True
+        self.pitch = 0
+        self.roll = 0
+        self.speed = 0.05
+        self.move ={
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False
+        }
         self.hero = loader.loadModel("block.egg")
         self.hero.setTexture(loader.loadTexture("skeleton_130x130.png"))
         self.hero.setPos(10,20,3)
@@ -91,7 +104,7 @@ class Hero():
         else:
             self.mode = True
 
-    def forward(self):
+    """    def forward(self):
         angle = self.hero.getH() % 360
         self.moveTo(angle)
 
@@ -112,21 +125,59 @@ class Hero():
 
     def rotateRight(self):
         self.hero.setH(self.hero.getH()-5)
+"""
+
+
+    def update_key(self,key,value):
+        self.move[key] = value
+
+
+    def update(self):
+        speedX = 0
+        speedY = 0
+        speedZ = 0
+
+        z = 0
+        if self.cameraOn:
+            if base.mouseWatcherNode.hasMouse():
+                x = base.mouseWatcherNode.getMouseX()
+                y = base.mouseWatcherNode.getMouseY()
+                prop = base.win.getProperties()
+                base.win.movePointer(0, prop.getXSize() // 2, prop.getYSize() // 2)
+                self.pitch -= x*30
+                self.hero.setH(self.pitch)
+                self.roll -= y*30
+                self.roll = max(min(self.roll,90), -90)
+                self.hero.setP(self.roll)
+            if self.move["forward"]:
+                speedX += cos(degreeToRadian(self.hero.getH() - 90))
+                speedY += cos(degreeToRadian(self.hero.getH() - 180))
+            if self.move["backward"]:
+                speedX += cos(degreeToRadian(self.hero.getH() - 270))
+                speedY += cos(degreeToRadian(self.hero.getH() - 0))
+            if self.move["left"]:
+                speedX += cos(degreeToRadian(self.hero.getH() - 0))
+                speedY += cos(degreeToRadian(self.hero.getH() - 90))
+            if self.move["forward"]:
+                speedX += cos(degreeToRadian(self.hero.getH() - 180))
+                speedY += cos(degreeToRadian(self.hero.getH() - 270))
+            
+
 
 
     def accept_events(self):
         base.accept(camera,self.camera_change)
         base.accept(mode,self.chageMode)
 
-        base.accept(rotate_left,self.rotateLeft)
-        base.accept(rotate_left + "-repeat",self.rotateLeft)
-        base.accept(rotate_right,self.rotateRight)
-        base.accept(rotate_right + "-repeat",self.rotateRight)
-        base.accept(forward, self.forward)
-        base.accept(forward + "-repeat",self.forward)
-        base.accept(backward, self.backward)
-        base.accept(backward + "-repeat",self.backward)
-        base.accept(left, self.left)
-        base.accept(left + "-repeat",self.left)
-        base.accept(right, self.right)
-        base.accept(right + "-repeat",self.right)
+        #base.accept(rotate_left,self.rotateLeft)
+        #base.accept(rotate_left + "-repeat",self.rotateLeft)
+        #base.accept(rotate_right,self.rotateRight)
+        #base.accept(rotate_right + "-repeat",self.rotateRight)
+        #base.accept(forward, self.forward)
+        #base.accept(forward + "-repeat",self.forward)
+        #base.accept(backward, self.backward)
+        #base.accept(backward + "-repeat",self.backward)
+        #base.accept(left, self.left)
+        #base.accept(left + "-repeat",self.left)
+        #base.accept(right, self.right)
+        #base.accept(right + "-repeat",self.right)
